f0ed8da2be99b5b8a79613f3c429e8a61efeab14
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-07 15:39:15 +0000

    Finished for loops test

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ *This program prints black and white according to user input 
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in); 
+    //ask user for a size of the line 
+    System.out.println("Please enter the length of the line"); 
+    int size = input.nextInt();  
+    int coma = size % 2; 
+    String pattern = "black, white, ";
     
+    
+    //use for loop to count up size 
+    for(int i = 3; i < size; i++){ 
+      System.out.print(pattern);
+      
+    }if(coma >= 1){ 
+      System.out.print("black");
+    }else{
+      System.out.print("black");
+    }
+  
+      
+ 
   }
 }
